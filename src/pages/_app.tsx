f4773caa5f977f562b4b0041d6e93660261098da
Jake Wang <wangzhiyi05@gmail.com>
2025-05-24 15:49:47 -0400

    add icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#1a1a2e" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
